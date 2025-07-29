abcc741e8f0eabc25d60b4c6a79e64d31fce5233
Praveen Kumar <120276801+praveen-202@users.noreply.github.com>
2025-07-29 11:24:09 +0530

    Update App.jsx

--- YouTubePlaylistDuration_Frontend/yt_playlist_Calculator/src/App.jsx
+++ YouTubePlaylistDuration_Frontend/yt_playlist_Calculator/src/App.jsx
@@ -1,14 +1,4 @@
-// import React from "react";
-// import PlaylistForm from "./components/PlaylistForm";
 
-// function App() {
-//   return (
-//     <div className="p-6 max-w-2xl mx-auto">
-//       <h2 className="text-2xl font-bold mb-4">YouTube Playlist Duration Calculator</h2>
-//       <PlaylistForm />
-//     </div>
-//   );
-// }
 
 // export default App;
 
